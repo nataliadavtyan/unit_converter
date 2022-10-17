9a00b1bb7fb62b1e3b8420c81811ff921d968f56
Natalia Davtyan <110120210+nataliadavtyan@users.noreply.github.com>
2022-10-17 16:55:29 +0200

    Volume field replaced by temperature

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
-
 let unitEl = document.getElementById("unit-el")
 const buttonEl = document.getElementById("button-el")
 let lengthEl = document.getElementById("length-el")
-let volumeEl = document.getElementById("volume-el")
+let temperatureEl = document.getElementById("temperature-el")
 let massEl = document.getElementById("mass-el")
 
+
 const metersToFeet = 3.281
-const literToGallons = 0.264
 const kilogramsToPounds = 2.204
 
 buttonEl.addEventListener("click", function(){
@@ -20,12 +14,16 @@ buttonEl.addEventListener("click", function(){
         ${baseValue} meters = ${(baseValue * metersToFeet).toFixed(3)} feet 
         | ${baseValue} feet = ${(baseValue / metersToFeet).toFixed(3)} meters
     `
-    volumeEl.textContent = `
-        ${baseValue} liters = ${(baseValue * literToGallons).toFixed(3)} gallons 
-        | ${baseValue} gallons = ${(baseValue / literToGallons).toFixed(3)} meters
+    temperatureEl.textContent = `
+        ${baseValue} °C = ${((baseValue * 9 / 5) + 32).toFixed(1)} °F 
+        | ${baseValue} °F = ${((baseValue - 32) * 5/9).toFixed(1)} °C
     `
     massEl.textContent = `
         ${baseValue} kilograms = ${(baseValue * kilogramsToPounds).toFixed(3)} pounds 
         | ${baseValue} pounds = ${(baseValue / kilogramsToPounds).toFixed(3)} kilograms
     `
+})
+
+unitEl.addEventListener("click", function(){
+    unitEl.value = ""
 })
