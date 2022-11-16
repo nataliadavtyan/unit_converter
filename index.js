490161065a3e8546c669f9f31fc5f1176b3b9578
Natalia Davtyan <110120210+nataliadavtyan@users.noreply.github.com>
2022-11-16 18:48:06 +0100

    Refactored the code

--- index.js
+++ index.js
@@ -6,34 +6,33 @@ const massEl = document.getElementById("mass-el")
 
 const metersToFeet = 3.281
 const kilogramsToPounds = 2.204
+const zeroValue = "0"
 
-basicState()
+render(zeroValue)
 
-function basicState(){
-    lengthEl.textContent = "0 meters = 0 feet | 0 feet = 0 meters"
-    temperatureEl.textContent = "0 °C = 32 °F | 0 °F = -17.8 °C"
-    massEl.textContent = "0 kilograms = 0 pounds | 0 pounds = 0 kilograms"
+function render(value){
+      if (value) {
+        lengthEl.textContent = `
+        ${value} meters = ${(value * metersToFeet).toFixed(3)} feet 
+        | ${value} feet = ${(value / metersToFeet).toFixed(3)} meters
+    `
+    temperatureEl.textContent = `
+        ${value} °C = ${((value * 9 / 5) + 32).toFixed(1)} °F 
+        | ${value} °F = ${((value - 32) * 5/9).toFixed(1)} °C
+    `
+    massEl.textContent = `
+        ${value} kilograms = ${(value * kilogramsToPounds).toFixed(3)} pounds 
+        | ${value} pounds = ${(value / kilogramsToPounds).toFixed(3)} kilograms
+    `
+    }
 }
 
 buttonEl.addEventListener("click", function(){
     let baseValue = unitEl.value
-    if (baseValue) {
-        lengthEl.textContent = `
-        ${baseValue} meters = ${(baseValue * metersToFeet).toFixed(3)} feet 
-        | ${baseValue} feet = ${(baseValue / metersToFeet).toFixed(3)} meters
-    `
-    temperatureEl.textContent = `
-        ${baseValue} °C = ${((baseValue * 9 / 5) + 32).toFixed(1)} °F 
-        | ${baseValue} °F = ${((baseValue - 32) * 5/9).toFixed(1)} °C
-    `
-    massEl.textContent = `
-        ${baseValue} kilograms = ${(baseValue * kilogramsToPounds).toFixed(3)} pounds 
-        | ${baseValue} pounds = ${(baseValue / kilogramsToPounds).toFixed(3)} kilograms
-    `
-    }
+    render(baseValue)
 })
 
 unitEl.addEventListener("click", function(){
     unitEl.value = ""
-    basicState()
+    render(zeroValue)
 })
